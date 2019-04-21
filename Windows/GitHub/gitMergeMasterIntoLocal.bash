b8ff8956dabb64ce9d67ec8424eb32ffc62db1c4
Ashish <42094101+Candy128x@users.noreply.github.com>
2019-04-21 20:49:57 +0530

    Add files via upload

---
+++ Windows/GitHub/gitMergeMasterIntoLocal.bash
@@ -0,0 +1,12 @@
+echo "Let's Merge master into local branch.."
+
+echo Whats your local branch name:
+read localBranch
+
+git checkout master
+git pull origin master
+git checkout $localBranch
+git merge master
+echo 'master branch succeessfuly merge into $localBranch'
+
+$SHELL
